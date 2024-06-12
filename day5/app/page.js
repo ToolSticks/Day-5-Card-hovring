daa5776536d5f52207c4ed73b7829ac8bbf3b2f4
Ajith Chakkadath <toolsticks@gmail.com>
2024-06-12 18:50:15 +0530

    updated

--- day5/app/page.js
+++ day5/app/page.js
@@ -2,6 +2,34 @@
 import { useState } from 'react';
 
 export default function Home() {
+
+  const data = [
+    {
+      title: 'services',
+      text: 'welcome to the services',
+      hoverText: 'the main services are web development, ui/ux design, web design',
+      backgroundColor: 'bg-blue-400'
+    },
+    {
+      title: 'services2',
+      text: 'welcome to the services2',
+      hoverText: 'the main services are web development2, ui/ux design2, web design2',
+      backgroundColor: 'bg-red-400'
+    },
+    {
+      title: 'services3',
+      text: 'welcome to the services3',
+      hoverText: 'the main services are web 3development, ui/ux design3, web design3',
+      backgroundColor: 'bg-red-400'
+    },
+    {
+      title: 'services4',
+      text: 'welcome to the services',
+      hoverText: 'the main3 services are web development, ui/ux design4, web design',
+      backgroundColor: 'bg-blue-400'
+    }
+  ];
+
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const handleMouseEnter = (index) => {
@@ -50,15 +78,20 @@ export default function Home() {
 
   return (
     <main className="p-24 min-h-screen min-w-screen  flex justify-center items-center">
-      <div className="relative h-[900px] w-[900px] bg-blue-400 grid grid-cols-2 grid-rows-2 p-5 gap-5 pb-10 ">
-        {[1, 2, 3, 4].map((item, index) => (
+      <div className="relative h-[900px] w-[900px]  grid grid-cols-2 grid-rows-2 p-5 gap-5 pb-10 ">
+        {data.map((item, index) => (
           <div
             key={index}
-            className={`border ${getDivClass(index)} ${getPositionClass(index)} ${getPositionClassvalue(index)} transition-transform duration-300 rounded-3xl`}
+            className={`border ${getDivClass(index)} ${getPositionClass(index)} ${getPositionClassvalue(index)} ${item.backgroundColor} transition-transform duration-300 rounded-3xl`}
             onMouseEnter={() => handleMouseEnter(index)}
             onMouseLeave={handleMouseLeave}
           >
-            {item}
+            <div className='p-10'>
+              <h1 className='text-3xl text-white pb-2'>{item.title}</h1>
+              <p className='text-2xl'>{hoveredIndex === index ? item.hoverText : item.text}</p>
+
+              </div>
+
           </div>
         ))}
       </div>
